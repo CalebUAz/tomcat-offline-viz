ed385add5cb4e4a193b842490a945750871d303a
CalebUAz <calebshibu@email.arizona.edu>
2022-11-13 00:10:23 -0700

    able to scrub through screenshots

--- slider_1.py
+++ slider_1.py
@@ -8,75 +8,43 @@ from utils import read_screenshots
 class Window(QWidget):
     def __init__(self):
         super().__init__()
-        self.title = "PyQt5 Slider"
+        self.title = "ToMCAT offline viz"
         self.top = 200
         self.left = 500
         self.width = 1400
         self.height = 1300
 
         self.InitWindow()
+
     
     def InitWindow(self):
-        # hbox = QHBoxLayout()
-
+        hbox = QHBoxLayout()
+        min, max, self.screenshots = read_screenshots()
         self.label = QLabel(self)
-        self.label.setPixmap(QPixmap('/Users/calebjonesshibu/Desktop/tom/pilot/exp_2022_11_08_11/tiger/screenshots/output000011.png'))
+        self.label.setPixmap(QPixmap(self.screenshots[0]))
         self.label.setGeometry(60, 60, 1280, 720)
         
         self.slider = QSlider(Qt.Horizontal, self)
         self.slider.setTickInterval(1)
-        self.slider.setMinimum(0)
-        self.slider.setMaximum(100)
+
+        self.slider.setMinimum(min)
+        self.slider.setMaximum(max)
         self.slider.setGeometry(100, 830, 1000, 20)
         self.slider.valueChanged[int].connect(self.changedValue)
-        # self.slider.setTickPosition(QSlider.TicksBelow)
-        # self.label = QLabel("0")
+        self.slider.setTickPosition(QSlider.TicksBelow)
 
         self.setWindowIcon(QtGui.QIcon("icon.png"))
         self.setWindowTitle(self.title)
         self.setGeometry(self.top, self.left, self.width, self.height)
 
-        # self.setLayout(hbox)
+        self.setLayout(hbox)
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.show()
 
-        # # self.setWindowIcon(QtGui.QIcon("icon.png"))
-        # # self.setWindowTitle(self.title)
-        # hbox = QHBoxLayout()
-
-
-        # self.label = QLabel(self)
-        # self.label.setPixmap(QPixmap('/Users/calebjonesshibu/Desktop/tom/pilot/exp_2022_11_08_11/tiger/screenshots/output000011.png'))
-        # self.label.setGeometry(60, 60, 1280, 720)
-
-        # self.slider = QSlider(Qt.Horizontal, self)
-        # self.slider.setGeometry(100, 60, 1340, 20)
-        # self.slider.valueChanged[int].connect(self.changedValue)
-                
-        # self.slider.setTickPosition(QSlider.TicksBelow)
-        # self.slider.setTickInterval(1)
-        # self.slider.setMinimum(0)
-        # self.slider.setMaximum(100)
-        # self.label = QLabel("0")
-        # # self.label.setFont(QtGui.QFont("Sanserif", 15))
-        # hbox.addWidget(self.slider)
-        # hbox.addWidget(self.label)
-        # self.setLayout(hbox)
-        # self.setGeometry(self.left, self.top, self.width, self.height)
-        # self.show()
-
     def changedValue(self, value):
         # change value of the slider when you move the slider and switch to the next img
-        if value == 0:
-            # print(value)
-            self.label.setPixmap(QPixmap('/Users/calebjonesshibu/Desktop/tom/pilot/exp_2022_11_08_11/tiger/screenshots/output033301.png'))
-        elif value < 50:
-            # print(value)
-            self.label.setPixmap(QPixmap('/Users/calebjonesshibu/Desktop/tom/pilot/exp_2022_11_08_11/tiger/screenshots/output054237.png'))
-        else:
-            # print(value)
-            self.label.setPixmap(QPixmap('/Users/calebjonesshibu/Desktop/tom/pilot/exp_2022_11_08_11/tiger/screenshots/output064216.png'))
-
+        self.label.setPixmap(QPixmap(self.screenshots[value]))
+        print(self.screenshots[value])
         # size = self.slider.value()
         # self.label.setText(str(size))
 
--- utils/_read_screenshots.py
+++ utils/_read_screenshots.py
@@ -3,7 +3,7 @@ import os
 def read_screenshots():
     screenshot_map = {}
     key = 0
-    folder_dir = "/Users/calebjonesshibu/Desktop/tom/pilot/exp_2022_11_08_11/tiger/screenshots/"
+    folder_dir = "./screenshots/"
     for images in os.listdir(folder_dir):
         # check if the image ends with png or jpg or jpeg
         key = key+1
@@ -11,5 +11,5 @@ def read_screenshots():
             or images.endswith(".jpeg")):
             # display
             screenshot_map[key] = folder_dir + images
-    print('Loaded SS')
-    return screenshot_map
+    print('Loaded Screenshots')
+    return list(screenshot_map.keys())[0], list(screenshot_map.keys())[-1], list(screenshot_map.values())
