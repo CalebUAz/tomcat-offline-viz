61d8e135040e6cf28ffe35343245f429ab0569fc
CalebUAz <calebshibu@email.arizona.edu>
2023-03-29 11:10:06 -0700

    readme and requirements added

---
+++ README.md
@@ -0,0 +1,20 @@
+# ToMCAT-Offline-Viz
+
+Install the necessary packages using `pip`
+```bash
+pip install -r requirements.txt
+```
+**or** using `conda`
+```bash
+conda install --file requirements.txt
+```
+
+## Launching the application
+Currently we are working in modules there you have execute screen_eye_tracker_slider.py and fnirs_slider.py seperatly using the following command:
+```bash
+python3 screenshot_eye_track_slider.py
+```
+**and** 
+```bash
+python3 fnirs_slider.py
+```
--- fnirs_slider.py
+++ fnirs_slider.py
@@ -1,186 +1,160 @@
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QSlider, QLabel
+from PyQt5.QtCore import Qt
+import pyqtgraph as pg
 import sys
 import pandas as pd
-import numpy as np
-from PyQt5.QtWidgets import QApplication, QMainWindow, QSlider, QLabel, QVBoxLayout, QWidget
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
 
-class MainWindow(QMainWindow):
-    def __init__(self, data):
-        super().__init__()
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
 
-        self.setWindowTitle("Data Plotting")
-        self.data = data
-        self.channels = len(self.data.columns)
+        self.channel_list = [
+            "S1-D1",
+            "S1-D2",
+            "S2-D1",
+            "S2-D3",
+            "S3-D1",
+            "S3-D3",
+            "S3-D4",
+            "S4-D2",
+            "S4-D4",
+            "S4-D5",
+            "S5-D3",
+            "S5-D4",
+            "S5-D6",
+            "S6-D4",
+            "S6-D6",
+            "S6-D7",
+            "S7-D5",
+            "S7-D7",
+            "S8-D6",
+            "S8-D7",
+        ]
+        data = pd.read_csv("/Users/calebjonesshibu/Desktop/tom/exp_2023_02_03_10/tiger/eeg_fnirs_pupil/eeg_fnirs_pupil/NIRS_filtered.csv",sep = '\t')
+        
+        self.data1 = data.iloc[:,1:21]
+        self.data2 = data.iloc[:,21:41]
 
-        # Create the plot widget
-        self.plot_widget = QWidget(self)
-        self.plot_layout = QVBoxLayout(self.plot_widget)
-        self.plot_canvas = FigureCanvas(Figure(figsize=(12, 12)))
-        self.plot_layout.addWidget(self.plot_canvas)
+        print(self.data1)
 
-        # Create the slider widget
-        self.slider = QSlider(self)
+        # initialize plots
+        super(MainWindow, self).__init__()
+
+        self.graphWidgetLayout = pg.GraphicsLayoutWidget()
+        self.graphWidgetLayout.resize(1000, 2500)
+        self.setCentralWidget(self.graphWidgetLayout)
+
+        self.slider = QSlider(Qt.Horizontal, self)
+        self.slider.setTickInterval(1)
+
         self.slider.setMinimum(0)
-        self.slider.setMaximum(len(self.data)-100)
-        self.slider.setOrientation(1)
-        self.slider.setTickInterval(100)
+        self.slider.setMaximum(1000)
+        self.slider.setGeometry(50, 1200, 1000, 20)
+        self.slider.valueChanged.connect(self.update_plot_data)
         self.slider.setTickPosition(QSlider.TicksBelow)
+        # Enable antialiasing for prettier plots
 
-        # Create the label widget
-        self.label = QLabel(self)
-        self.label.setText("Current Index: 0")
+        pg.setConfigOptions(antialias=True)
 
-        # Add the slider and label widgets to the main layout
-        main_layout = QVBoxLayout()
-        main_layout.addWidget(self.slider)
-        main_layout.addWidget(self.label)
-        main_layout.addWidget(self.plot_widget)
-        central_widget = QWidget()
-        central_widget.setLayout(main_layout)
-        self.setCentralWidget(central_widget)
+        self.graphWidgetLayout.setBackground("w")
 
-        # Connect the slider signal to the update plot function
-        self.slider.valueChanged.connect(self.update_plot)
+        self.pen = pg.mkPen(color=(255, 0, 0), width=2)  # red for HbO
+        self.pen1 = pg.mkPen(color=(0, 0, 255), width=2)  # blue for HbR
 
-        # Update the plot for the initial slider value
-        self.update_plot(0)
+        self.ch = []
+        self.ch1 = []
 
-    def update_plot(self, value):
-        # Get the current index from the slider value
-        idx = value
+        label_style = {"color": (255, 0, 0), "font-size": "10pt"}
 
-        # Clear the previous plot
-        self.plot_canvas.figure.clf()
+        # self.srate = 10  # 10.2Hz for NIRS data
+        # self.timer = QtCore.QTimer()
+        # # why? https://stackoverflow.com/questions/59094207/how-to-set-pyqt5-qtimer-to-update-in-specified-interval
+        # self.timer.setInterval(round(1000 / self.srate))
 
-        # Create 20 subplots
-        fig, axs = plt.subplots(nrows=20, sharex=True, figsize=(10, 20))
+        n_channels = len(self.channel_list)
 
-        # Plot the data for each channel in a separate subplot
-        for i in range(self.channels):
-            channel_data = self.data.iloc[idx:idx+100, i]
-            axs[i].plot(channel_data)
-            axs[i].set_title(f"Channel {i+1}")
-            axs[i].set_ylabel("Amplitude")
+        self.x = [0]
+        self.y = [[0] for _ in range(n_channels)]  # HbO
+        self.y1 = [[0] for _ in range(n_channels)]  # HbR
 
-        # Set the x-axis label for the last subplot
-        axs[-1].set_xlabel("Samples")
+        self.dataLine = [[] for _ in range(n_channels)]
+        self.dataLine1 = [[] for _ in range(n_channels)]
 
-        # Set the title for the entire plot
-        fig.suptitle("Data Plot")
+        for self.idx, self.channel in enumerate(self.channel_list):
+            # create 20 subplots
 
-        # Draw the plot
-        self.plot_canvas.draw()
+            self.channel = self.graphWidgetLayout.addPlot(row=self.idx, col=0)
+            # self.channel.showAxes('left', showValues=False)
 
-        # Update the label text
-        self.label.setText(f"Current Index: {idx}")
+            if self.idx < n_channels - 1:
+                self.channel.hideAxis("bottom")
 
+            self.channel.setLabel("left", self.channel_list[self.idx], **label_style)
+
+            self.ch.append(self.channel)
+            self.ch1.append(self.channel)
+
+        self.plots()
+
+    def plots(self):
+        # draw
+
+        for self.idx, (self.ch, self.ch1) in enumerate(zip(self.ch, self.ch1)):
+            self.ch = self.ch.plot(x=self.x, y=self.y[self.idx], pen=self.pen)
+            self.ch1 = self.ch1.plot(x=self.x, y=self.y1[self.idx], pen=self.pen1)
+
+            self.dataLine[self.idx].append(self.ch)
+            self.dataLine1[self.idx].append(self.ch1)
+
+        # self.timer.timeout.connect(self.update_plot_data)
+        # self.timer.start()
+
+    def update_slider_value(self, value):
+        self.slider_value = value
+        
+
+    def update_plot_data(self, value):
+        self.slider_value = value
+        # update data
+
+        if len(self.x) >= 100:
+            self.x = self.x[1:]  # Remove the first x element.
+
+            for i in range(len(self.channel_list)):
+                self.y[i] = self.y[i][1:]  # Remove the first
+                self.y1[i] = self.y1[i][1:]  # Remove the first
+
+        # Get the next chunk of samples from LSL.
+        # They were accumulated while we were plotting the previous chunk
+        # sample, time = self.inlet.pull_chunk()
+
+        if len(self.data1) > 0:
+            # Plot the most recent sample of this chunk. Discard the rest
+
+            # Update the x value according to the number of samples we skipped
+            self.x.append(self.x[-1] + len(self.data1.iloc[-1].values))
+
+            # Append the last sample
+            for i in range(len(self.channel_list)):
+                # print(self.data1[-1][i], self.data2[-1][i])
+                if self.slider_value == 0:
+                    print('Im less than 0')
+                    self.y[i].append(self.data1.iloc[-1][i])
+                    self.y1[i].append(self.data2.iloc[-1][i])
+                else:
+                    print('Im greater than 0')
+                    start = int(len(self.data1) * (self.slider_value / 1000))
+                    end = int(len(self.data1) * ((self.slider_value + 1) / 1000))
+                    self.y[i].append(self.data1.iloc[start:end, i].mean())
+                    self.y1[i].append(self.data2.iloc[start:end, i].mean())
+                    print(self.data1.iloc[start:end, i], self.data2.iloc[start:end, i])
+
+            for i in range(0, len(self.channel_list)):
+                self.dataLine[i][0].setData(self.x, self.y[i])
+                self.dataLine1[i][0].setData(self.x, self.y1[i])
 
 if __name__ == "__main__":
-    # Load data from CSV file
-    data = pd.read_csv("/Users/calebjonesshibu/Desktop/tom/exp_2023_02_03_10/tiger/eeg_fnirs_pupil/eeg_fnirs_pupil/NIRS_filtered.csv",sep = '\t')
-    data = data.iloc[:,1:20]
-
-    # Create the Qt application and main window
-    app = QApplication(sys.argv)
-    main_window = MainWindow(data)
-
-    # Show the main window and start the event loop
-    main_window.show()
+    app = QtWidgets.QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
     sys.exit(app.exec_())
-
-# import sys
-# import pandas as pd
-# import numpy as np
-# from PyQt5.QtWidgets import QApplication, QMainWindow, QSlider, QLabel, QVBoxLayout, QWidget
-# from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-# from matplotlib.figure import Figure
-
-# class MainWindow(QMainWindow):
-#     def __init__(self, data):
-#         super().__init__()
-
-#         self.setWindowTitle("Data Plotting")
-#         self.data = data
-#         self.channels = len(self.data.columns)
-
-#         # Create the plot widget
-#         self.plot_widget = QWidget(self)
-#         self.plot_layout = QVBoxLayout(self.plot_widget)
-#         self.plot_canvas = FigureCanvas(Figure(figsize=(5, 3)))
-#         self.plot_layout.addWidget(self.plot_canvas)
-
-#         # Create the slider widget
-#         self.slider = QSlider(self)
-#         self.slider.setMinimum(0)
-#         self.slider.setMaximum(len(self.data)-100)
-#         self.slider.setOrientation(1)
-#         self.slider.setTickInterval(100)
-#         self.slider.setTickPosition(QSlider.TicksBelow)
-
-#         # Create the label widget
-#         self.label = QLabel(self)
-#         self.label.setText("Current Index: 0")
-
-#         # Add the slider and label widgets to the main layout
-#         main_layout = QVBoxLayout()
-#         main_layout.addWidget(self.slider)
-#         main_layout.addWidget(self.label)
-#         main_layout.addWidget(self.plot_widget)
-#         central_widget = QWidget()
-#         central_widget.setLayout(main_layout)
-#         self.setCentralWidget(central_widget)
-
-#         # Connect the slider signal to the update plot function
-#         self.slider.valueChanged.connect(self.update_plot)
-
-#         # Update the plot for the initial slider value
-#         self.update_plot(0)
-
-#     def update_plot(self, value):
-#         # Get the current index from the slider value
-#         idx = value
-
-#         # Clear the previous plot
-#         self.plot_canvas.figure.clf()
-
-#         # Create a new plot
-#         ax = self.plot_canvas.figure.add_subplot(111)
-
-#         # Plot the data for each channel
-#         for i in range(self.channels):
-#             channel_data = self.data.iloc[idx:idx+100, i]
-#             ax.plot(channel_data)
-
-#         # Set the title and axis labels
-#         ax.set_title("Data Plot")
-#         ax.set_xlabel("Samples")
-#         ax.set_ylabel("Amplitude")
-
-#         # Draw the plot
-#         self.plot_canvas.draw()
-
-#         # Update the label text
-#         self.label.setText(f"Current Index: {idx}")
-
-# if __name__ == "__main__":
-#     # Load data from CSV file
-#     data = pd.read_csv("/Users/calebjonesshibu/Desktop/tom/exp_2023_02_03_10/tiger/eeg_fnirs_pupil/eeg_fnirs_pupil/NIRS_filtered.csv",sep = '\t')
-#     data = data.iloc[:,1:20]
-
-#     # Create the Qt application and main window
-#     app = QApplication(sys.argv)
-#     main_window = MainWindow(data)
-
-#     # Show the main window and start the event loop
-#     main_window.show()
-#     sys.exit(app.exec_())
-
-
-
-# data = pd.read_csv("/Users/calebjonesshibu/Desktop/tom/exp_2023_02_03_10/tiger/eeg_fnirs_pupil/eeg_fnirs_pupil/NIRS_filtered.csv",sep = '\t')
-# data = data.iloc[:,1:20]
-
-# self.data = pd.read_csv("/Users/calebjonesshibu/Desktop/tom/exp_2023_02_03_10/tiger/eeg_fnirs_pupil/eeg_fnirs_pupil/NIRS_filtered.csv",sep = '\t')
-# self.data = self.data.iloc[:,1:20].to_numpy()
