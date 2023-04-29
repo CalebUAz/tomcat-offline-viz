8a5dcfa9927fd626cca49a97a2444e21d243d4f7
Harshita Narnoli <harshitanarnoli@arizona.edu>
2023-04-28 19:07:50 -0700

    layout change and eye-tracking in box 1

--- home.py
+++ home.py
@@ -1,26 +1,74 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QHBoxLayout, QVBoxLayout, QTabWidget
+import time
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QHBoxLayout, QVBoxLayout, QTabWidget, QSpacerItem, QSizePolicy, QSlider
+import cv2
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap, QImage
+from matplotlib.widgets import SliderBase
+
+from screenshot_eye_track_slider import Window
 
 
 class MyWidget(QWidget):
     def __init__(self):
         super().__init__()
-        self.width = 1000
-        self.height = 900
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(self.width, self.width, self.height, self.height)  # Set the position and size of the widget
+        self.setGeometry(200, 200, 400, 300)  # Set the position and size of the widget
 
-        hbox = QHBoxLayout()
-        vbox = QVBoxLayout()
+        label1 = QLabel("eye-tracking")
+        label2 = QLabel("fNIRS")
+        label3 = QLabel("Button to switch")
+        label4 = QLabel("Slider")
 
-        self.tab_widget = QTabWidget()
-        self.tab_widget2 = QTabWidget()
-        vbox.addWidget(self.tab_widget)
-        vbox.addWidget(self.tab_widget2)
+        # Add a border style to each label
+        label1.setStyleSheet("border: 1px solid black;")
+        label2.setStyleSheet("border: 1px solid black;")
+        label3.setStyleSheet("border: 1px solid black;")
+        label4.setStyleSheet("border: 1px solid black;")
 
-        # self.button = QPushButton('Click me', self)  # Create a button
+        # Set custom dimensions for each label
+        label1.setFixedSize(1100, 800)
+        label2.setFixedSize(300, 700)
+        label3.setFixedSize(200, 90)
+        label4.setFixedSize(1400, 50)
+
+        # Create the layout managers
+        main_layout = QVBoxLayout()
+
+        # create the first row layout which includes eye-tracking and signal view
+        sub_layout1 = QHBoxLayout()
+        eye_tracking = QVBoxLayout()
+        fNIRS_EEG = QVBoxLayout()
+
+        # Set the stretch factor for the main layout to make box 1 take 2/3 of the vertical space
+        main_layout.addLayout(sub_layout1)
+
+        # Set the stretch factor for box 2 and box 3 to divide the remaining vertical space equally
+        sub_layout1.addLayout(eye_tracking, 2)
+        sub_layout1.addLayout(fNIRS_EEG)
+
+        # Create the second row layout
+        sub_layout2 = QHBoxLayout()
+        slider = QVBoxLayout()
+
+        # Set the stretch factor for the second row layout to divide the vertical space equally
+        main_layout.addLayout(sub_layout2)
+
+        # Set the stretch factor for box 1 in the second row layout to make it 2/3 of the row's width
+        sub_layout2.addLayout(slider, 2)
+
+        window = Window()
+
+        # Add the labels to the respective layouts
+        eye_tracking.addWidget(window)
+        fNIRS_EEG.addWidget(label2)
+        fNIRS_EEG.addWidget(label3)
+        slider.addWidget(label4)
+        
+        # self.button = QPushButton('Click me', self)  
+        # Create a button
 
         # vbox.addWidget(self.button)
         # self.button.clicked.connect(self.on_button_clicked)  # Connect the button's clicked signal to a function
@@ -29,11 +77,36 @@ class MyWidget(QWidget):
         # self.label.setText('Button not clicked yet')  # Set the initial text for the label
         # self.label.move(10, 50)  # Move the label to a specific position
 
+        self.setLayout(main_layout)
+
         self.show()  # Show the widget
 
-    def on_button_clicked(self):
-        self.label.setText('Button clicked')  # Update the label's text when the button is clicked
+    # def on_button_clicked(self):
+    #     self.label.setText('Button clicked')  # Update the label's text when the button is clicked
 
+
+    # def changedValue(self, value):
+    #     # change value of the slider when you move the slider and switch to the next img
+    #     start = time.process_time()
+
+    #     rgb_image = cv2.cvtColor(self.screenshots[value], cv2.COLOR_BGR2RGB)
+    #     # print(self.x[value], self.y[value])
+    #     x_value = self.x.get(value, None)
+    #     y_value = self.y.get(value, None)
+
+    #     if x_value is not None and y_value is not None:
+    #         cv2.circle(rgb_image, (int(x_value), int(y_value)),
+    #                    5, (255, 0, 0), 2)
+
+    #     h, w, ch = rgb_image.shape
+    #     bytes_per_line = ch * w
+    #     convert_to_Qt_format = QtGui.QImage(
+    #         rgb_image, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
+    #     self.ScreenShot.setPixmap(QPixmap.fromImage(convert_to_Qt_format))
+
+    #     val = self.slider.value()
+    #     self.slider_text.setText(str(val))
+    #     print('Time taken by changedValue:', time.process_time() - start)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- screenshot_eye_track_slider.py
+++ screenshot_eye_track_slider.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from PyQt5 import QtGui
-from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QSlider, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QSlider, QLabel, QVBoxLayout
 import sys
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap, QImage
@@ -8,24 +8,19 @@ from utils import read_screenshots, read_pupil_data
 import time
 import cv2
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QTabWidget, QTextEdit, QPushButton
+
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
         self.title = "ToMCAT offline viz"
-        self.top = 200
-        self.left = 500
-        self.width = 1400
-        self.height = 1300
+        self.top = 10
+        self.left = 10
+        self.width = 1100
+        self.height = 800
         self.InitWindow()
 
     def InitWindow(self):
-        # min, max, self.screenshots = read_screenshots()
-        # print(min, max)
-
-        # self.x, self.y, point_scale, id_labels = read_pupil_data()
-
         hbox = QHBoxLayout()
 
         # Create a vertical layout for the button
@@ -34,23 +29,23 @@ class Window(QWidget):
         self.slider = QSlider(Qt.Horizontal, self)
         self.slider.setTickInterval(1)
 
-        # create the tab widget and add it to the layout
-        self.tab_widget = QTabWidget()
-        vbox.addWidget(self.tab_widget)
-
         self.slider.setMinimum(0)
         self.slider.setMaximum(50)
-        self.slider.setGeometry(150, 80, 1000, 20)
+        self.slider.setGeometry(200, 2000, 100, 20)
         self.slider.sliderMoved[int].connect(self.changedValue)
         self.slider.setTickPosition(QSlider.TicksBelow)
 
         self.slider_text = QLabel(self)
-        # self.tab_widget.addTab(self.slider_text, "Subject-1")
-        self.slider_text.setGeometry(100, 860, 900, 20)
+        self.slider_text.setGeometry(200, 850, 150, 20)
 
-        # self.ScreenShot = QLabel(self)
-        # # self.ScreenShot.setPixmap(QPixmap(self.screenshots[0]))
-        # self.ScreenShot.setGeometry(60, 60, 1280, 720)
+        min, max, self.screenshots = read_screenshots()
+        print(min, max)
+
+        self.x, self.y, point_scale, id_labels = read_pupil_data()
+
+        self.ScreenShot = QLabel(self)
+        # self.ScreenShot.setPixmap(QPixmap(self.screenshots[0]))
+        self.ScreenShot.setGeometry(100, 100, 1280, 720)
 
         # Add the slider to the vertical layout
         vbox.addWidget(self.slider)
@@ -62,14 +57,11 @@ class Window(QWidget):
         self.setGeometry(self.top, self.left, self.width, self.height)
 
         self.setLayout(hbox)
-        # self.setGeometry(self.top, self.left, self.width, self.height)
+        self.setGeometry(self.left, self.top, self.width, self.height)
 
-        # self.tab2 = QPushButton("Click me")
-        self.addTabSubject("Subject-1")
-        self.addTabSubject("Subject-2")
+        self.ScreenShot = QLabel(self)
+        self.ScreenShot.setGeometry(100, 100, 1280, 720)
 
-        # breakpoint()
-        
         self.show()
 
     def changedValue(self, value):
@@ -82,30 +74,21 @@ class Window(QWidget):
         y_value = self.y.get(value, None)
 
         if x_value is not None and y_value is not None:
-            cv2.circle(rgb_image, (int(x_value), int(y_value)), 5, (255, 0, 0), 2)
+            cv2.circle(rgb_image, (int(x_value), int(y_value)),
+                       5, (255, 0, 0), 2)
 
         h, w, ch = rgb_image.shape
         bytes_per_line = ch * w
-        convert_to_Qt_format = QtGui.QImage(rgb_image, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
+        convert_to_Qt_format = QtGui.QImage(
+            rgb_image, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
         self.ScreenShot.setPixmap(QPixmap.fromImage(convert_to_Qt_format))
 
         val = self.slider.value()
         self.slider_text.setText(str(val))
         print('Time taken by changedValue:', time.process_time() - start)
 
-    def addTabSubject(self, subject):
-        min, max, self.screenshots = read_screenshots()
-        print(min, max)
 
-        self.x, self.y, point_scale, id_labels = read_pupil_data()
-
-        self.tab_widget.addTab(self.slider_text, subject)
-        self.ScreenShot = QLabel(self)
-        # self.ScreenShot.setPixmap(QPixmap(self.screenshots[0]))
-        self.ScreenShot.setGeometry(60, 60, 1280, 720)
-        # breakpoint()
-
-# if __name__ == '__main__':
-#     App = QApplication(sys.argv)
-#     window = Window()
-#     sys.exit(App.exec_())
+if __name__ == '__main__':
+    App = QApplication(sys.argv)
+    window = Window()
+    sys.exit(App.exec_())
